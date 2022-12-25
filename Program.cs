bdd3ffad14d3375359c807641385d6134abc2d54
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-25 13:51:41 +0300

    in progress 52

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 // Например, задан массив:
-
 // 1 4 7 2
-
 // 5 9 2 3
-
 // 8 4 2 4
-
-// 17 -> такого числа в массиве нет
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 int [,] GetMatrix(int m, int n)
 {
     int [,] matrix = new int[m,n];
@@ -23,20 +18,17 @@ int [,] GetMatrix(int m, int n)
     return matrix;
 }
 
-void GetElement(int [,] array, int A, int B)
+double [,] GetColumnsSum(double [,] array)
 {
-    if (A > array.GetLength(0) || B > array.GetLength(1)) System.Console.WriteLine("Такого элемента в массиве нет");
-    else 
+    double columnSum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                if (i==A && j==B) System.Console.WriteLine($"Ваш элемент в массиве = {array [i,j]}");
-            }
+            columnSum+=array[i,j];
         }
     }
-    
+    return array;
 }
 
 void PrintMatrix(int [,] InputMatrix)
@@ -60,11 +52,4 @@ int [,] result = GetMatrix(rows, columns);
 System.Console.WriteLine("Двумерный массив");
 PrintMatrix(result);
 
-System.Console.WriteLine("Введите номер строки элемента в массиве:");
-int element0 = Convert.ToInt32(Console.ReadLine());
-
-System.Console.WriteLine("Введите номер столбца элемента в массиве:");
-int element1 = Convert.ToInt32(Console.ReadLine());
-
-GetElement(result, element0-1, element1-1);
 
